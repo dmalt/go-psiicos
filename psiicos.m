0270bc08b00c73a78d2bd1158401a98ffac5ad37
Dmitriy Altukhov <dm-altukhov@ya.ru>
2015-02-18 15:29:29 +0300

    Started to write main loop

--- psiicos.m
+++ psiicos.m
@@ -1,13 +1,13 @@
-% Initialization
 %% IrMxNE: linear regression with mixed norm regularization
 function X = IrMxNE(M, G)
-
+	
+	% Initialization
 	S = 10 % Number of sources
 	T = 10 % Number of samples
 
-	X = zeros(S, T); % Init our matrix of interest
-	W = diag( ones(S * T, 1) ); % Init weights matrix
-	w = ones(S * T, 1); % Weights vector
+	X = zeros(S, T); % Matrix of signal sources; 
+	w = ones(S, 1); % Init weights vector
+	W = eye(S); % Init weights matrix 
 
 	lambda = 1. % Regularization parameter
 	epsilon = 1. % Dual gap threshold
@@ -15,6 +15,13 @@ function X = IrMxNE(M, G)
 	I = 100 % Number of BCD iterations per MxNE iteration
 	K = 100 % Number of MxNE iterations
 
+	for k = 1:K
+		W = inv(diag(w));
+		G = G * W;
+		l = zeros(S,1);
+		for s = 1:S
+			fprintf("hi\n");
+		end
+	end
 
-
-	X = ;
+	X = 1;
