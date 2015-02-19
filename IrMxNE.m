2c57a4d0ce6d8563fdfcc7145f541d2828ea64dc
Dmitriy Altukhov <dm.altukhov@ya.ru>
2015-02-19 19:12:50 +0300

    Finished program but it doesn`t work

--- IrMxNE.m
+++ IrMxNE.m
@@ -1,25 +1,30 @@
 %% IrMxNE: linear regression with mixed norm regularization
-%function X = IrMxNE(M, G)
+%% function X = IrMxNE(M, G)
 	
 	% Initialization
-	S = 10; 	% Number of sources
-	T = 10; 	% Number of samples
-	Sen = 2;	% Number of sensors
+	S = 50; 	% Number of sources
+	T = 25; 	% Number of samples
+	Sen = 10;	% Number of sensors
 
-	X = zeros(S, T);	% Matrix of signal sources; 
+	X_prev = zeros(S, T);	% Matrix of signal sources; 
+	X_next = zeros(S, T);	% Matrix of signal sources; 
 	w = ones(S, 1); 	% Init weights vector
 	W = eye(S); 		% Init weights matrix 
 
-	lambda = 1.; 	% Regularization parameter
+	lambda = 100.; 	% Regularization parameter
 	epsilon = 1e-6;	% Dual gap threshold
 	tau = 1e-6;  	% Tolerance 
 	I = 100;		% Number of BCD iterations per MxNE iteration
-	K = 100;		% Number of MxNE iterations
+	K = 500;		% Number of MxNE iterations
 
-	M = zeros(Sen, T) % Measurement. M should be passed from outside
-	G = zeros(Sen, S) % Gain matrix
+	G = rand(Sen, S)*100; % Gain matrix
+	Answer = zeros(S, T);
+	ix = randint(1,5, [1,S]);
+	Answer(ix,:) = rand(5, T) * 100.;
+	M = G * Answer; %rand(Sen, T); % Measurements. Should be passed from the outside
 
 	for k = 1:K
+		X_prev = X_next;
 		W = inv(diag(w));
 		G = G * W;
 		l = zeros(S, 1);
@@ -36,11 +41,24 @@
 
 		for i = 1:I
 			for s = 1:S
+				Y_prev = Y_next;
 				Y_next(s,:) = Y_prev(s,:) + mu(s) .* G(:,s)' * R;
-				Y_next(s,:) = Y_next(s,:) * (  1 - mu(s) * lambda / norm( Y_next(s,:), 2 )  );
+				Y_next(s,:) = Y_next(s,:) * (  1. - mu(s) * lambda / norm( Y_next(s,:), 2 )  );
 				R = R - G(:,s) * ( Y_next(s,:) - Y_prev(s,:) );
 			end
+			% Should compute a dual gap here and check for the convergence 
 		end
-
+		X_next = W * Y_next;
+		for s = 1:S
+			w(s) = 1. / (   2. * sqrt(  norm( X_next(s,:) )  )   );
+		end
+		if norm(X_next - X_prev, inf) < tau
+			fprintf('breaked');
+			break;
+		end
 	end
-	X = 1;
+	
+			% Should compute dual gap here and check for convergence 
+	norm(X_next, 'fro')
+	norm(Answer, 'fro')
+	norm(X_next - Answer, 'fro')
