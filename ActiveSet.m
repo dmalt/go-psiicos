6407fd264c66e17428aee13e3be3df5d3072e313
Dmitriy Altukhov <dm.altukhov@ya.ru>
2015-04-06 16:48:17 +0300

    Added ActiveSet.m and calc_violations.c

---
+++ ActiveSet.m
@@ -0,0 +1,29 @@
+%% subset: returns an array of indices of sources which violate KKT conditions the most
+function A = ActiveSet(G_small, R, lambda, w, k)
+	fprintf('Calculating active set...\n');
+	V = 200; % Number of sources in a subset
+	[ans, S] = size(G_small);
+	if k == 1
+		S = 2 * S ^ 2; 
+		idx = @(x) x;
+	elseif k ~= 1
+		idx = support(w);
+		[dummy, S] = size(idx);
+	end
+
+
+	matlabpool('open', 4);
+	src_violations(S) = 0; % Memory allocation. 
+	parfor s = 1:S
+		src_violations(s)  = norm(columnG_fast(idx(s),G_small, w)' * R) - lambda;
+	end	
+	matlabpool close;
+	[Dummy, A] =  sort(src_violations, 'descend');
+	if V < S
+		A = A(1:V);
+	end
+	A = A(src_violations(A) > 0);
+	src_violations(A)
+	A = idx(A);
+	fprintf('Done\n');
+	A = sort(A);
---
+++ calc_violations.c
@@ -0,0 +1,80 @@
+#include "mex.h"
+
+void columnG_fast(mwSize p, double * G_small, double * w, mwSize Nsen, mwSize Nsrc, double * G_col);
+void calc_violations(double * G_small, double * w, mwSize Nsen, mwSize Nsrc, double lambda, double * R, mwSize T, double * violations);
+double calc_product_norm(double * col, double * R, mwSize Sn, mwSize T);
+
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+	if(nrhs != 4)
+    	mexErrMsgIdAndTxt("MyToolbox:arrayProduct:nrhs", "Four inputs required.");
+    double * G_small = mxGetPr(prhs[0]);
+ 	mwSize Nsen = mxGetM(prhs[0]);
+ 	mwSize Nsrc = mxGetN(prhs[0]);
+ 	double * w = mxGetPr(prhs[1]);
+ 	double lambda = mxGetScalar(prhs[2]);
+ 	double * R = mxGetPr(prhs[3]);
+ 	mwSize T = mxGetN(prhs[3]);
+
+ 	double * violations;
+ 	plhs[0] = mxCreateDoubleMatrix(Nsrc * Nsrc * 2, 1, mxREAL);
+	violations = mxGetPr(plhs[0]);
+	calc_violations(G_small, w, Nsen, Nsrc, lambda, R, T, violations);
+}
+
+void calc_violations(double * G_small, double * w, mwSize Nsen, mwSize Nsrc, double lambda, double * R, mwSize T, double * violations)
+{
+	mwSize Sr = Nsrc * Nsrc * 2;
+	mwSize Sn = Nsen * Nsen * 2; /* Number of elements in a column */
+	mwSize s;
+	double * column = mxGetPr(mxCreateDoubleMatrix(Sn,1,mxREAL));
+	double norm = 0.;
+	for (s = 0; s < 1e6; ++s)
+	{
+		columnG_fast(s+1, G_small, w, Nsen, Nsrc, column);
+		norm = calc_product_norm(column, R, Sn, T);
+		violations[s] = norm - lambda;
+		/*if(!(s % 10000))
+			mexPrintf("s = %d\n", s);*/
+	}
+/*	mxDestroyArray(column);
+*/}
+
+double calc_product_norm(double * col, double * R, mwSize Sn, mwSize T)
+{
+	double * product = mxGetPr(mxCreateDoubleMatrix(T,1,mxREAL));
+	mwSize t, sn;
+	double norm = 0;
+	for (t = 0; t < 1; ++t)
+	{
+		product[t] = 0;
+		for (sn = 0; sn < Sn; ++sn)
+			product[t] += col[sn] * R[sn + Sn * t];
+		norm += product[t] * product[t];
+	}
+	/*mxDestroyArray(product);*/
+	norm = sqrt(norm);
+	return norm;
+}
+
+void columnG_fast(mwSize p, double * G_small, double * w, mwSize Nsen, mwSize Nsrc, double * G_col)
+{
+	mwSize q = p % (Nsrc * Nsrc);
+	if(!q)
+		q = Nsrc * Nsrc;
+
+	mwSize i = q % Nsrc;
+	if(!i)
+		i = Nsrc;
+	i --;
+	mwSize j = (q - i) / Nsrc;
+	double w_p = w[p-1];
+	mwSize k, l;
+	for (k = 0; k < Nsen; ++k)
+		for (l = 0; l < Nsen; ++l)
+		{
+			G_col[k + Nsen * l] = G_small[k + Nsen * i] * G_small[l + Nsen * j] * w_p;
+			G_col[Nsen * Nsen + k + Nsen * l] = G_col[k + Nsen * l];
+		}
+}
