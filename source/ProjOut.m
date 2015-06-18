eed1419f4268ded0b828c3ae2653acc781552cf4
Dmitriy Altukhov <dm.altukhov@ya.ru>
2015-06-18 21:41:21 +0300

    Renamed G2dU to G

--- source/ProjOut.m
+++ source/ProjOut.m
@@ -1,25 +1,24 @@
-function [ Cp ] = ProjOut(CT, CT2, G2dU)
+function [ Cp ] = ProjOut(CT, CT2, G)
 
 % if(nargin < 3)
     Rnk  = 350;
-    Rnk12 = 20;
 % end;
 
-Ns = size(G2dU, 2) / 2; % two topography columns per each source of the grid
+Ns = size(G, 2) / 2; % two topography columns per each source of the grid
 % perform projection of the coherence matrix away from the power only
-Nch = size(G2dU, 1);
+Nch = size(G, 1);
 % component
 % vecC = CT(:); % Vectorize first
 vecC = CT;
 % project for each potential source
-NsHR = size(G2dU, 2) / 2; % two topography columns per each source of the grid
+NsHR = size(G, 2) / 2; % two topography columns per each source of the grid
 range3 = 1:3;
 fprintf('Projecting cross-spectral data away from the power only component\n');
 
 A = zeros(Nch ^ 2, Ns * 3);
 for i = 1:NsHR
     range_i = i * 2 - 1:i * 2;
-    gi = G2dU(:,range_i);
+    gi = G(:,range_i);
     v = gi(:,1) * gi(:,1)';
     V(:,1) = v(:);
     v = gi(:,2) * gi(:,2)';
@@ -40,6 +39,7 @@ vecCp  = vecC - Upwr * Upwr' * vecC;
 clear AA A;
 Cp = vecCp;
 if(~isempty(CT2))
+    Rnk12 = 20;
     CT2  = CT2-Upwr * (Upwr' * CT2);
     [u2,s2,v2] = svd(CT2,'econ');
     Cp = Cp-u2(:,1:Rnk12)*(u2(:,1:Rnk12)'*Cp); 
