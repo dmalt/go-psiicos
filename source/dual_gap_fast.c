cd2309be0a5aa461fa6cbb7cec51ae2bdc31d35f
Dmitriy Altukhov <dm.altukhov@ya.ru>
2015-06-22 17:45:06 +0300

    Learned to multiply matrices in dual_gap_fast.c

--- source/dual_gap_fast.c
+++ source/dual_gap_fast.c
@@ -5,6 +5,9 @@
 }*/
 	#include <cblas.h>
 
+void CalcDualGap(mwSize, mwSize, mwSize, double *, double *, double *, double *, double *);
+
+
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
 	if(nrhs != 6)
@@ -16,20 +19,19 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 
  	mwSize Nsrc_sq = mxGetN(prhs[1]);
  	double * G = mxGetPr(prhs[1]);
-
  	double * X = mxGetPr(prhs[2]);
-
  	mwSize lambda = mxGetScalar(prhs[3]);
-
  	mwSize S = mxGetScalar(prhs[4]);
-
  	double * R = mxGetPr(prhs[5]);
+ 	plhs[0] = mxCreateDoubleMatrix(Nsen_sq,Ntime,mxREAL);
+ 	double * temp = malloc(sizeof(double));
+ 	temp = mxGetPr(plhs[0]);
+ 	CalcDualGap(Nsrc_sq, Ntime, Nsen_sq, M, G, X, R, temp);
 }
 
-
-CalcDualGap(mwSize Nsrc_sq, mwSize Ntime, mwSize Nsen_sq, double * M, double * G, double * X, double * R)
+void CalcDualGap(mwSize Nsrc_sq, mwSize Ntime, mwSize Nsen_sq, double * M, double * G, double * X, double * R, double * temp)
 {
-	double * temp = malloc(sizeof(double));
-	cblas_dgemv(CblasRowMajor, CblasNoTrans, Ch, Ch, 1, colRmat[t], Ch, G[2 * i], 1, 0., temp, 1);
-	return 0;
+	/* cblas_dgemv(CblasColMajor, CblasNoTrans, Nsen_sq, Nsrc_sq, 1, G, Nsen_sq, X, 1, 0., temp, 1); */
+	cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, Nsen_sq, Ntime, Nsrc_sq, 1, G, Nsen_sq, X, Nsrc_sq, 0, temp, Nsen_sq);
 }
+
