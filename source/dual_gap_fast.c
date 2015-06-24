b065ab11fe985a1a68d5b069dab8d808f840ee5e
Dmitriy Altukhov <dm.altukhov@ya.ru>
2015-06-24 20:35:55 +0300

    dual_gap_fast works

--- source/dual_gap_fast.c
+++ source/dual_gap_fast.c
@@ -27,7 +27,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
  	mwSize Nsrc_sq 	= mxGetN(prhs[1]);
  	double * G 		= mxGetPr(prhs[1]);
  	double * X 		= mxGetPr(prhs[2]);
- 	mwSize lambda 	= mxGetScalar(prhs[3]);
+ 	double lambda 	= mxGetScalar(prhs[3]);
  	mwSize S 		= mxGetScalar(prhs[4]);
  	double * R 		= mxGetPr(prhs[5]);
 
@@ -44,7 +44,7 @@ double CalcDualGap(mwSize Nsrc_sq, mwSize Ntime, mwSize Nsen_sq, mwSize S, doubl
 	double sigma = 0.;
 	double * B = (double *)mxMalloc(sizeof(double) * S);
 	double * R_ = (double *)mxMalloc(sizeof(double) * Nsen_sq * Ntime);
-	double C = 0;
+	double C = 0.;
 	double * G_s = (double *)mxMalloc(sizeof(double) * Nsen_sq * 4); /* G_s corresponds to topography of two interacting sites on cortex, 
 	i.e. two interacting pairs of dipoles*/
 	double maxC_1 = 1.;
@@ -55,16 +55,16 @@ double CalcDualGap(mwSize Nsrc_sq, mwSize Ntime, mwSize Nsen_sq, mwSize S, doubl
 		G_s = &G[Nsen_sq * src * 4];
 		X_s = &X[Ntime * src * 4];
 		/* G(:,range)' * R: */
-		cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, 4, Ntime, Nsen_sq , 1, G_s, 4, R, Nsen_sq, 0, temp, Nsen_sq);
+		cblas_dgemm(CblasColMajor, CblasTrans, CblasNoTrans, 4, Ntime, Nsen_sq , 1, G_s, Nsen_sq, R, Nsen_sq, 0, temp, 4);
 		B[src] = cblas_dnrm2(4 * Ntime, temp, 1);
-		mexPrintf("%f\n", B[src]);
+		/*mexPrintf("%f\n", B[src]);*/
 		sigma += cblas_dnrm2(4 * Ntime, X_s, 1); 
 	}
 	C = max_array(B, S) / lambda;
-	mexPrintf("%f\n", C);
-	mexPrintf("%f\n", lambda);
+	/*mexPrintf("%f\n", C);
+	mexPrintf("lambda = %f\n", lambda);
 	mexPrintf("%f\n", max_array(B,S));
-
+*/
 	if(C > 1.)
 		maxC_1 = C;
 	cblas_dcopy(Nsen_sq * Ntime, R, 1, R_, 1);
@@ -74,6 +74,10 @@ double CalcDualGap(mwSize Nsrc_sq, mwSize Ntime, mwSize Nsen_sq, mwSize S, doubl
 
 	double normR_ = normR / maxC_1; 
 	double R_dotM = cblas_ddot(Nsen_sq * Ntime, R_, 1, M, 1); /* = sum(sum(R_ .* M )) */
+	/*mexPrintf("R_dotM = %f\n", R_dotM);
+	mexPrintf("normR = %f\n", normR);
+	mexPrintf("normR_ = %f\n", normR_);
+	mexPrintf("sigma = %f\n", sigma);*/
 	double eta = 0.5 * normR * normR + lambda * sigma + 0.5 * normR_ * normR_ - R_dotM;
 	return eta;
 	/*cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, Nsen_sq, Ntime, Nsrc_sq, 1, G, Nsen_sq, X, Nsrc_sq, 0, temp, Nsen_sq);*/
