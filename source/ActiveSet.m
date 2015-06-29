cee8cd41ee2e57a08eccc479b422369b468a7c03
Dmitriy Altukhov <dm.altukhov@ya.ru>
2015-06-29 22:00:13 +0300

    Fixed error in ActiveSet.m

--- source/ActiveSet.m
+++ source/ActiveSet.m
@@ -2,35 +2,35 @@
 function A = ActiveSet(G_small, R_, lambda, w, k, V)
 	fprintf('Calculating active set...\n');
 	fprintf('Start\n');
-	if k == 1	
-		matlabpool('open', 4);
-		src_violations(S) = 0; % Memory allocation. 
-		parfor s = 1:S
-			src_violations(s)  = norm(columnG_fast(idx(s),G_small, w)' * R) - lambda;
-		end	
-		matlabpool close;
-		tic;
-		R = [real(R_), imag(R_)];
+	startBCD = tic;
+	R = [real(R_), imag(R_)];
+	if k == 1		
+		idx = @(x) x;
+        S = length(w);
 		save ../aux/G_small.txt G_small -ASCII
 		save ../aux/R.txt R -ASCII
 		save ../aux/w.txt w -ASCII
 		unix('./ActSetViol');
 		src_violations = load('../aux/V.txt');
 		fprintf('Stop. ');
-		toc;
 		src_violations = src_violations' - lambda;
 	elseif k ~= 1
 		idx = support(w);
 		[dummy, S] = size(idx);
+				src_violations(S) = 0; % Memory allocation. 
+		for s = 1:S
+			src_violations(s)  = norm(G_pair(idx(s),G_small, w(idx(s)))' * R, 'fro') - lambda;
+		end	
 	end
 	[Dummy, A] =  sort(src_violations, 'descend');
-	% if V < S
+	if V < S
 		 A = A(1:V);
-	% end
+	end
 	
 	% src_violations(1:100)
 	A = A(src_violations(A) > 0);
 	% src_violations(A)
-	% A = idx(A);
+	A = idx(A);
+	toc(startBCD);
 	fprintf('Done\n');
 	A = sort(A);
