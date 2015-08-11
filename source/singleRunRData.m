44bf1d31331c85a832a13fe17d9cc7cfa40deb45
Dmitriy Altukhov <dm.altukhov@ya.ru>
2015-08-11 19:21:29 +0300

    Added scripts to run a single (non bootstrap) trial of go_psiicos and to draw the result
    	new file:   drawscripts/drawSingleRun.m
    	new file:   singleRunRData.m

---
+++ source/singleRunRData.m
@@ -0,0 +1,9 @@
+% and perform single run of go_psiicos
+% function [X,A] = singleRunRData(Subj)
+% X - recovered timecourses
+% A - recovered interaction pairs
+
+function [X,A] = singleRunRData(Subj)
+	[ConData, G] = PrepRealData(Subj);
+	CT = CrossSpectralTimeseries(ConData{2}.Trials );
+	[X,A] = go_psiicos(0.05, 500, G, CT, ConData{1}.CrossSpecTime);
