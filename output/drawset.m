a2d6d24b4a705105ec7a21c55c191894b60881dd
Dmitriy Altukhov <dm.altukhov@ya.ru>
2015-08-11 18:29:21 +0300

    Removed superfluous scripts. Their current versions are in source/drawscript 	deleted:    output/bfs.m 	deleted:    output/drawlcurve.m 	deleted:    output/drawset.m 	deleted:    output/sparse_to_csr.m

--- output/drawset.m
+++
@@ -1,8 +0,0 @@
-function res = drawset(A, R)
- col = randint(1,3);
-for i = 1:size(A,1)
-		L1 = line( R(A(i,:), 1), R(A(i,:),2), R(A(i,:),3) );
-		plot3( R(A(i,1), 1), R(A(i,1),2), R(A(i,1),3),'.', 'Color', col, 'Markersize', 40 );
-        plot3( R(A(i,2), 1), R(A(i,2),2), R(A(i,2),3),'.', 'Color', col, 'Markersize', 40 );
-    	set(L1, 'Color', col, 'linewidth',2);
-end
